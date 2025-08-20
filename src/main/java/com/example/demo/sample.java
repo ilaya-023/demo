a37d8f1690fe022a5713299df2e655303e4ed1a2
ilaya-023 <ilayarajasir023@gmail.com>
2025-08-20 17:09:34 +0530

    Update sample.java

--- src/main/java/com/example/demo/sample.java
+++ src/main/java/com/example/demo/sample.java
@@ -5,6 +5,7 @@ import java.sql.SQLOutput;
 public class sample {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("hello Ilaya");
 
     }
 
