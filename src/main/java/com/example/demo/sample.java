bb879fcf83952c6ccb5c8ea716034ceb9e0c417d
ilaya-023 <ilayarajasir023@gmail.com>
2025-08-20 17:53:55 +0530

    Update sample.java

--- src/main/java/com/example/demo/sample.java
+++ src/main/java/com/example/demo/sample.java
@@ -5,6 +5,10 @@ import java.sql.SQLOutput;
 public class sample {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+          System.out.println("Hello World!");
+          System.out.println("Hello World!");
+          System.out.println("Hello World!");
+          System.out.println("Hello World!");
 
 
     }
